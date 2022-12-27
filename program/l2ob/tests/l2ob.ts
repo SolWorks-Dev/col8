bf5f509c0904f1246abbe0fe3567378e782b138e
Zhe <zhesolworks@gmail.com>
2022-12-27 00:56:02 +0000

    Add test for initialisation

--- program/l2ob/tests/l2ob.ts
+++ program/l2ob/tests/l2ob.ts
@@ -1,16 +1,36 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { BN } from "bn.js";
 import { L2ob } from "../target/types/l2ob";
 
 describe("l2ob", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
   const program = anchor.workspace.L2ob as Program<L2ob>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  it("Initialize orderbook", async () => {
+    const orderbookAcc = anchor.web3.Keypair.generate();
+    const auth = (program.provider as anchor.AnchorProvider).wallet;
+
+    const tx = await program.methods
+      .initialize(
+        auth.publicKey,
+        "DEGODS-SOL",
+        "DEGODS",
+        "SOL",
+        new BN(0),
+        new BN(0),
+        0,
+        0
+      )
+      .accounts({
+        l2Orderbook: orderbookAcc.publicKey,
+        authority: auth.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
     console.log("Your transaction signature", tx);
+
+    const ob = await program.account.l2Orderbook.fetch(orderbookAcc.publicKey);
+    console.log("Orderbook", ob);
   });
 });
