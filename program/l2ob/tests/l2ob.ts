b14242256dcf15b4731e889f02c9ff5e11b60479
Zhe <zhesolworks@gmail.com>
2022-12-28 13:27:40 +0000

    Fix initialization test, add tests for failing cases with invalid param values

--- program/l2ob/tests/l2ob.ts
+++ program/l2ob/tests/l2ob.ts
@@ -1,13 +1,14 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { assert } from "chai";
-import { SDK } from "../SDK";
+import { Decimal, SDK } from "../SDK";
 import { L2ob } from "../target/types/l2ob";
 
 describe("l2ob", () => {
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
   const program = anchor.workspace.L2Ob as Program<L2ob>;
+  const sdk = new SDK(program);
 
   it("Initialize orderbook", async () => {
     const auth = provider.publicKey;
@@ -15,42 +16,73 @@ describe("l2ob", () => {
     const quote = "SOL";
     const market = `${base}-${quote}`;
     const minimumSizeIncrement = 1;
-    const sizeExponent = 1;
-    const minimumPriceIncrement = 0.1;
-    const priceExponent = 2;
+    const sizeExponent = 0;
+    const minimumPriceIncrement = 10;
+    const priceExponent = -2;
 
-    const sdk = new SDK(program);
-    const {
-      orderbook
-    } = await sdk.initialize({
+    const { orderbook } = await sdk.initialize({
       admin: auth,
       base,
       quote,
-      minimumSizeIncrement: {
-        value: minimumSizeIncrement,
-        exponent: sizeExponent,
-      },
-      minimumPriceIncrement: {
-        value: minimumPriceIncrement,
-        exponent: priceExponent,
-      },
+      minimumSizeIncrement: new Decimal(minimumSizeIncrement, sizeExponent),
+      minimumPriceIncrement: new Decimal(minimumPriceIncrement, priceExponent),
     });
 
+    console.log(JSON.stringify(await sdk.getOrderbook(orderbook)));
     const ob = await program.account.l2Orderbook.fetch(orderbook);
-    console.log(JSON.stringify(ob, null, 2));
     assert.equal(ob.baseCurrencyName, base);
     assert.equal(ob.quoteCurrencyName, quote);
     assert.equal(ob.marketName, market);
-    assert.equal(
-      ob.minimumSizeIncrement.toNumber(),
-      minimumSizeIncrement * 10 ** ob.sizeExponent
-    );
+    assert.equal(ob.minimumSizeIncrement.toNumber(), minimumSizeIncrement);
     assert.equal(ob.sizeExponent, sizeExponent);
-    assert.equal(
-      ob.minimumPriceIncrement.toNumber(),
-      minimumPriceIncrement * 10 ** ob.priceExponent
-    );
+    assert.equal(ob.minimumPriceIncrement.toNumber(), minimumPriceIncrement);
     assert.equal(ob.priceExponent, priceExponent);
     assert.equal(auth.toBase58(), ob.authority.toBase58());
+  });
+
+  it("Try initialize orderbook with base name", async () => {
+    const auth = provider.publicKey;
+    const base = "";
+    const quote = "SOL";
+    const minimumSizeIncrement = 1;
+    const sizeExponent = 0;
+    const minimumPriceIncrement = 10;
+    const priceExponent = -2;
+
+    try {
+      await sdk.initialize({
+        admin: auth,
+        base,
+        quote,
+        minimumSizeIncrement: new Decimal(minimumSizeIncrement, sizeExponent),
+        minimumPriceIncrement: new Decimal(minimumPriceIncrement, priceExponent),
+      });
+      assert.fail("Should have failed");
+    } catch {
+      assert.ok(true);
+    }
+  });
+
+  it("Try initialize orderbook with quote name", async () => {
+    const auth = provider.publicKey;
+    const base = "DEGODS";
+    const quote = "";
+    const minimumSizeIncrement = 1;
+    const sizeExponent = 0;
+    const minimumPriceIncrement = 10;
+    const priceExponent = -2;
+
+    try {
+      await sdk.initialize({
+        admin: auth,
+        base,
+        quote,
+        minimumSizeIncrement: new Decimal(minimumSizeIncrement, sizeExponent),
+        minimumPriceIncrement: new Decimal(minimumPriceIncrement, priceExponent),
+      });
+      assert.fail("Should have failed");
+    } catch {
+      assert.ok(true);
+    }
   });
 });
