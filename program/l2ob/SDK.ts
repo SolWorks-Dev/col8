9d24a99d24212439cb370d55c764d4b9a8ac446f
Zhe <zhesolworks@gmail.com>
2022-12-28 13:25:29 +0000

    Add decimal, orderbook types and getOrderbook method for SDK

--- program/l2ob/SDK.ts
+++ program/l2ob/SDK.ts
@@ -1,11 +1,49 @@
 import { Program } from "@project-serum/anchor";
 import { L2ob } from "./target/types/l2ob";
 import { PublicKey, SystemProgram, Keypair } from "@solana/web3.js";
-import { BN } from "bn.js";
+import BN from "bn.js";
 
-export type Decimal = {
+// class for decimal type
+export class Decimal {
   value: number;
   exponent: number;
+  constructor(value: number, exponent: number) {
+    this.value = value;
+    this.exponent = exponent;
+  }
+  getRealValue() {
+    return this.value * 10 ** this.exponent;
+  }
+  getValue() {
+    return this.value;
+  }
+  getExponent() {
+    return this.exponent;
+  }
+}
+
+export type Orderbook = {
+  orderbook: PublicKey;
+  authority: PublicKey;
+  marketName: string;
+  baseCurrencyName: string;
+  quoteCurrencyName: string;
+  minimumSizeIncrement: number;
+  sizeExponent: number;
+  minimumPriceIncrement: number;
+  priceExponent: number;
+  createdAt: number;
+  updatedAt: number;
+  bids: {
+    price: number;
+    size: number;
+  }[];
+  asks: {
+    price: number;
+    size: number;
+  }[];
+  isInitialized: boolean;
+  isDeprecated: boolean;
 };
 
 export class SDK {
@@ -36,12 +74,8 @@ export class SDK {
         market,
         base,
         quote,
-        new BN(
-          minimumSizeIncrement.value * 10 ** minimumSizeIncrement.exponent
-        ),
-        new BN(
-          minimumPriceIncrement.value * 10 ** minimumPriceIncrement.exponent
-        ),
+        new BN(minimumSizeIncrement.value),
+        new BN(minimumPriceIncrement.value),
         minimumPriceIncrement.exponent,
         minimumSizeIncrement.exponent
       )
@@ -56,5 +90,47 @@ export class SDK {
       orderbook: orderbookAcc.publicKey,
       txId,
     };
+  }
+
+  async getOrderbook(orderbook: PublicKey) {
+    const ob = await this._program.account.l2Orderbook.fetch(orderbook);
+    const typedBids = (ob.bids as [BN, BN][]).map((bid) => {
+      return {
+        price: bid[0].toNumber() * 10 ** ob.priceExponent,
+        size: bid[1].toNumber() * 10 ** ob.sizeExponent,
+      };
+    });
+    const typedAsks = (ob.asks as [BN, BN][]).map((ask) => {
+      return {
+        price: ask[0].toNumber() * 10 ** ob.priceExponent,
+        size: ask[1].toNumber() * 10 ** ob.sizeExponent,
+      };
+    });
+
+    const typedOb: Orderbook = {
+      orderbook: orderbook,
+      authority: ob.authority,
+      marketName: ob.marketName,
+      baseCurrencyName: ob.baseCurrencyName,
+      quoteCurrencyName: ob.quoteCurrencyName,
+      minimumSizeIncrement: new Decimal(
+        ob.minimumSizeIncrement.toNumber(),
+        ob.sizeExponent
+      ).getRealValue(),
+      sizeExponent: ob.sizeExponent,
+      minimumPriceIncrement: new Decimal(
+        ob.minimumPriceIncrement.toNumber(),
+        ob.priceExponent
+      ).getRealValue(),
+      priceExponent: ob.priceExponent,
+      createdAt: ob.createdAt.toNumber(),
+      updatedAt: ob.updatedAt.toNumber(),
+      bids: typedBids,
+      asks: typedAsks,
+      isInitialized: ob.isInitialized,
+      isDeprecated: ob.isDeprecated,
+    };
+
+    return typedOb;
   }
 }
